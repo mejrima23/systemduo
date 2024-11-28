f95fac1b2af97d819e089ebee5e84687b4cf7943
Mejrima <62061958+mejrima23@users.noreply.github.com>
2024-11-28 12:29:37 +0100

    tests for registration and field validation

--- cypress/e2e/specs/registration.spec.cy.js
+++ cypress/e2e/specs/registration.spec.cy.js
@@ -23,7 +23,7 @@ describe('Registration tests', () => {
     cy.get('form[action*="signup"]').should('be.visible')
   })
 
-  it.only('Succesfull registration', () => {
+  it('Succesfull registration', () => {
     // When
     cy.get('a[href*="login"]').should('be.visible').click()
 
@@ -65,5 +65,83 @@ describe('Registration tests', () => {
     cy.get('[data-qa="account-created"]')
       .should('be.visible')
       .and('contain.text', 'Account Created!')
+  })
+
+  it('Succesfull registration and login', () => {
+    // When
+    cy.get('a[href*="login"]').should('be.visible').click()
+
+    // Then
+    cy.get('.signup-form').should('be.visible')
+
+    // When
+    cy.get('[data-qa="signup-name"]').clear().type('Mejrima')
+    cy.get('[data-qa="signup-email"]').clear().type(email)
+    cy.get('[data-qa="signup-button"]').click()
+
+    // Then
+    cy.get('form[action*="signup"]').should('be.visible')
+
+    // When
+    cy.get('input[type="radio"]').should('be.visible').check('Mrs')
+    cy.get('[data-qa="email"]')
+      .should('be.disabled')
+      .and('have.attr', 'value', email)
+    cy.get('[data-qa="password"]').clear().type('Test123')
+    cy.get('[data-qa="days"]').select(7)
+    cy.get('[data-qa="months"]').select(12)
+    cy.get('[data-qa="years"]').select('1992')
+    cy.get('#newsletter').check()
+    cy.get('#optin').check()
+    cy.get('[data-qa="first_name"]').clear().type('Mejrima')
+    cy.get('[data-qa="last_name"]').clear().type('Ičanović')
+    cy.get('[data-qa="company"]').clear().type('QA')
+    cy.get('[data-qa="address"]').clear().type('Adresa')
+    cy.get('[data-qa="country"]').select('Canada')
+    cy.get('[data-qa="state"]').clear().type('Sarajevo')
+    cy.get('[data-qa="city"]').clear().type('Sarajevo')
+    cy.get('[data-qa="zipcode"]').clear().type('71000')
+    cy.get('[data-qa="mobile_number"]').clear().type('123456789')
+
+    cy.get('[data-qa="create-account"]').should('be.visible').click()
+    //Then
+    cy.get('[data-qa="account-created"]')
+      .should('be.visible')
+      .and('contain.text', 'Account Created!')
+
+    //When
+    cy.get('[data-qa="continue-button"]').should('be.visible').click()
+    cy.get('a[href*="logout"]').should('be.visible').click()
+    cy.get('[data-qa="login-email"]').clear().type(email)
+    cy.get('[data-qa="login-password"]').clear().type('Test123')
+    cy.get('[data-qa="login-button"]').should('be.visible').click()
+    //Then
+    cy.get('a[href*="logout"]').should('be.visible')
+  })
+  it('Succesfull registration and login', () => {
+    // When
+    cy.get('a[href*="login"]').should('be.visible').click()
+
+    // Then
+    cy.get('.signup-form').should('be.visible')
+
+    // When
+    cy.get('[data-qa="signup-name"]').clear().type('Mejrima')
+    cy.get('[data-qa="signup-email"]').clear().type(email)
+    cy.get('[data-qa="signup-button"]').click()
+
+    // Then
+    cy.get('form[action*="signup"]').should('be.visible')
+
+    // When
+    cy.get('input[type="radio"]').should('be.visible').check('Mrs')
+    cy.get('[data-qa="email"]')
+      .should('be.disabled')
+      .and('have.attr', 'value', email)
+    cy.get('[data-qa="create-account"]').should('be.visible').click()
+    //Then
+    cy.get('[data-qa="password"]')
+      .invoke('prop', 'validationMessage')
+      .should('equal', 'Please fill out this field.')
   })
 })
