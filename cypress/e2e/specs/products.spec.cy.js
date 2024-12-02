d246a79112f7fdfb5e7091646302ca7eed14935a
Mejrima <62061958+mejrima23@users.noreply.github.com>
2024-12-02 13:40:54 +0100

    tests for login with registration function

--- cypress/e2e/specs/products.spec.cy.js
+++ cypress/e2e/specs/products.spec.cy.js
@@ -24,21 +24,4 @@ describe('Contact us tests', () => {
     cy.get('@wantedProduct').parents('.product-image-wrapper').find('.choose a')
     cy.get('@wantedProduct').trigger('mouseenter')
   })
-  it.only('Add product to cart', () => {
-    cy.get('.single-products')
-      .should('be.visible')
-      .find('img[src*="product_picture/1"]')
-      .should('be.visible')
-    // .find('h2')
-    // .should('contain', 'Rs. 500')
-    cy.get('a[href*="/product_details/1"]').should('be.visible').click()
-  })
-  // it.only('Add product to cart', () => {
-  //   cy.contains('Blue Top')
-  //   .parents('.single-products')
-  //   .find('.productinfo')
-  //   .as('wantedProduct')
-  // cy.get('@wantedProduct').should('be.visible')
-
-  // })
 })
